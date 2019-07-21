8d07f1517b710ea86cda1f67d9a83ba6c1bc5d63
Xueyu Yin <yinxueyu@hotmail.com>
2019-07-21 11:54:51 -0400

    builder

---
+++ zero/Builder/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+	public static void main(String argv[]) {
+		NodeBuilder builder = new NodeBuilder();
+		Node node = null;
+		node = builder.setData("test").setNext(null).build();
+
+	}
+}
