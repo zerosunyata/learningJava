8c3f8df788fa813500ad5b8aaa1ec5823490fc34
Xueyu Yin <yinxueyu@gmail.com>
2019-07-28 17:36:07 -0400

    howto ch02

---
+++ zero/HowTo/Chapter02/Welcome4.java
@@ -0,0 +1,13 @@
+/*
+ *       Filename:  Welcome4.java
+ *
+ *    Description:  Fig. 2.6: Displaying multiple lines with method
+ *                  System.out.printf
+ *
+ * =====================================================================================
+ */
+public class Welcome4{
+    public static void main(String[] args){
+        System.out.printf("%s\n%s\n", "Welcome to ", "Java Programming!");
+    }
+}
