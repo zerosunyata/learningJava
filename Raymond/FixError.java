36b91a22ca2e77e6309a51487eca2bd374137ff1
Raymond Dai <RaymondD214@users.noreply.github.com>
2019-07-28 20:43:59 -0400

    Homework

---
+++ Raymond/FixError.java
@@ -0,0 +1,53 @@
+/**
+ * Read Textbook Chapter 1,2,3,4,5
+ * @author Xueyu Yin
+ *
+ */
+public class FixError {
+	/**
+	 * Find the error in each of the following code segments, and explain how to
+	 * correct it:
+	 */
+
+	public static void main(String[] args) {
+		a();
+		b();
+		c(1);
+		d();
+	}
+
+	public static void a() {
+
+		int i = 1;
+		while (i <= 10) {
+			++i;
+			System.out.println(i);
+		}
+	}
+
+	public static void b() {
+		for (double k = 0.1; k != 1.0; k += 0.1)
+			System.out.println(k);
+	}
+
+	public static void c(int n) {
+		switch (n) {
+		case 1:
+			System.out.println("The number is 1");
+			break;
+		case 2:
+			System.out.println("The number is 2");
+			break;
+		default:
+			System.out.println("The number is not 1 or 2");
+			break;
+		}
+	}
+
+	public static void d() {
+//		The following code should print the values 1 to 10:
+		int n = 1;
+		while (n <= 10) {
+			System.out.println(n++);
+	}
+}
